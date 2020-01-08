7ae8a407b17c2b8028f59de419115f23cae7f41f
QsJiain <jiajin@vip.qq.com>
2020-01-08 10:56:57 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,12 +4,16 @@ yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/d
 yum makecache fast
 rpm --import https://mirrors.aliyun.com/docker-ce/linux/centos/gpg
 yum -y install docker-ce
-systemctl enable docker
-systemctl start docker
 #国内加速
 curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+
+#启动docker
+systemctl enable docker
+systemctl restart docker
+
 #docker-compose
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+#输出版本
 docker -v
 docker-compose --version

311b31d35a94a9312278e1f466f0bf4d6faf82c8
QsJiain <jiajin@vip.qq.com>
2020-01-08 10:19:14 +0800

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast
+rpm --import https://mirrors.aliyun.com/docker-ce/linux/centos/gpg
+yum -y install docker-ce
+systemctl enable docker
+systemctl start docker

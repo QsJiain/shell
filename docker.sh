b17659ccbc2eb78f09999bfd22757c20d8b0cb7d
QsJiain <jiajin@vip.qq.com>
2020-01-08 10:31:03 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,3 +6,6 @@ rpm --import https://mirrors.aliyun.com/docker-ce/linux/centos/gpg
 yum -y install docker-ce
 systemctl enable docker
 systemctl start docker
+
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose

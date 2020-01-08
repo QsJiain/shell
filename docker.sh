24f1808c91465867d2ae505290dca62f20c6c637
QsJiain <jiajin@vip.qq.com>
2020-01-08 10:54:54 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,7 +6,9 @@ rpm --import https://mirrors.aliyun.com/docker-ce/linux/centos/gpg
 yum -y install docker-ce
 systemctl enable docker
 systemctl start docker
-
+#国内加速
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+#docker-compose
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker -v

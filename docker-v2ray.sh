49b73040f730ef02445b49b596ab7080c5297f40
QsJiain <jiajin@vip.qq.com>
2020-01-13 11:16:55 +0800

    Create docker-v2ray.sh

---
+++ docker-v2ray.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#第一次写sh 
+#创建一个镜像 提取配置文件
+#mkdir -p /etc/v2ray
+#docker run --rm --name v2temp -d v2ray/official
+#docker cp v2temp:/etc/v2ray/config.json /etc/v2ray/config.json
+#docker stop v2temp
+#!/bin/bash
+
+function funWithParam(){
+	json='{"v":"2","ps":"","add":"{ip}","port":"{port}","id":"{uuidy}","aid":"64","net":"tcp","type":"none","host":"","path":"","tls":""}'
+	json=${json//'{ip}'/"$1"}
+	json=${json//'{port}'/"$2"}
+	json=${json//'{uuidy}'/"$3"}
+	b64=$(base64 <<< "$json")
+	echo "vmess://$b64" | awk 'BEGIN{RS="\n";ORS="";}{print $0}'
+}
+#设置端口 如果有传入则替换
+port="8001"
+if [ "$1" ]; then
+ port=$1
+ fi
+ 
+isrun=$(docker ps -aq --filter name=v2ray)
+if [ -n "$isrun" ]; then
+ temp=$(docker stop v2ray && docker rm v2ray)
+ echo '容器已存在->正在删除' $isrun
+ fi
+ 
+UUID=$(cat /proc/sys/kernel/random/uuid)
+#启动容器
+dockerid=$(docker run --name v2ray -p:$port:8001 -d v2ray/official)
+#替换uuid
+docker exec v2ray sed -i "s/60ca58e9-003e-4c01-98de-c2223ae49153/${UUID}/g" "/etc/v2ray/config.json"
+#重启容器
+docker restart v2ray
+myip=$(curl -s whatismyip.akamai.com)
+echo ------------------------------------------------
+echo IP			:		$myip
+echo UUID		:		$UUID
+echo PORT		:		$port
+echo $(funWithParam "$myip" "$port" "$UUID")
+echo ------------------------------------------------

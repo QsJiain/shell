833741e5eda94a5a9768de46bfbab5acddbeafa8
QsJiain <jiajin@vip.qq.com>
2020-08-16 16:31:27 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-curl https://get.docker.com/ | sh
+yum install -y yum-utils device-mapper-persistent-data lvm2 epel-release
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast
+rpm --import https://mirrors.aliyun.com/docker-ce/linux/centos/gpg
+yum -y install docker-ce
 #阿里国内加速
 curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s https://vveeo4eo.mirror.aliyuncs.com
 
